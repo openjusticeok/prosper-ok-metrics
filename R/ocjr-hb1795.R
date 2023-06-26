4b5f094ef9917701a93ea542514085b688383fea
polina <prozhkova@uchicago.edu>
2023-06-26 10:57:15 -0700

    Create ocjr-hb1795.R

---
+++ R/ocjr-hb1795.R
@@ -0,0 +1,78 @@
+library(ojodb)
+library(tidyverse)
+library(lubridate)
+library(here)
+
+# notes from call
+# cases_of_interest <- ojo_tbl("case") |> filters to limit to misd drug charges
+# ojo_tbl("minute") |> 
+# all our filters for dl susp |> filter(case_id %in% !!cases_of_interest$id)
+
+reporting_start_date <- ymd("2022-01-01")
+reporting_end_date <- ymd("2023-04-01")
+
+case_m <- ojo_crim_cases(case_types = "CM",
+                     file_years = 2022:2023) |> 
+  collect()
+
+min <- ojo_tbl("minute") |> 
+  filter(case_type == "CM",
+         date_filed >= reporting_start_date,
+         date_filed < reporting_end_date) |> 
+  select(id,
+         case_id, 
+         date,
+         code,
+         description,
+         count,
+         amount) |> 
+  collect()
+
+# shortcut?
+cm <- ojo_crim_cases(case_types = "CM", 
+                     file_years = 2022:2023) |> 
+  ojo_add_minutes() |> 
+  collect() 
+  
+drugs <- "CDS|C\\.D\\.S|DRUG|OXY|HUFF|AMPHET|ZOLOL|ZOLAM|HYDROC|CODEIN|PRECURS|XANAX|MORPH|METERDI|ZEPAM|LORAZ|VALIU|EPHED|SUB|COCA|PSEUDO| CS|CS | CD|CD |PRESCRIP|NARC|METH|C\\.D\\.|HEROIN|ANHYD|AMMONIA|OPIUM|LORTAB|PARAPHERNALIA|MARIJUANA|MARIHUANA|MJ"
+
+drug_cm <- cm |> 
+  filter(str_detect(count_as_filed, drugs)
+         )
+
+################
+# useful regex
+drug_charge = if_else(grepl(drugs, count_as_filed), TRUE, FALSE)
+min_rev <- min |>
+   filter(
+     code %in% c("ABST", "NOSPSe", "NOWS", "NOSRT", "TEXT", "CNOTE", "NOSUS", "NO", "RULE8", "STAY", "WRCI"),
+     str_detect(description, "(DPS|DEPARTMENT OF PUBLIC SAFETY|D\\.P\\.S)"),
+     !str_detect(description, "(WITHDRAW|ERROR|RETURN|UNABLE|REVOKE|LIFT|RECALL|NOT PROCESSED|PENDING|SUSPENSION RELEASE|DEFENDANT APPEARS)"
+                 )
+   )
+
+filter(min_code %in% c("ABST", "NOSPSe", "NOWS", "NOSRT", "TEXT",
+                       "CNOTE", "NOSUS", "NO", "RULE8", "STAY", "WRCI"),
+       str_detect(min_desc, "SUSPENSION"),
+       !str_detect(min_desc, "(WITHDRAW|ERROR|RETURN|REVOKE|UNABLE|LIFT|NOT PROCESSED|PENDING|SUSPENSION RELEASE|DEFENDANT APPEARS)"))
+
+
+# Grouping / summarizing the charges by case
+cm_cases <- cm_charges |>
+  group_by(case_number, district) |>
+  summarize(
+    file_date = unique(date_filed),
+    file_year = unique(file_year),
+    quarter_fy = unique(quarter_fy),
+    quarter_fy_start_date = unique(quarter_fy_start_date),
+    pre_november = if_else(sum(pre_november, na.rm = T) > 0, TRUE, FALSE),
+    n_charges = n(),
+    drug_charge_present = if_else(sum(drug_charge, na.rm = T) > 0, TRUE, FALSE),
+    all_drugs = if_else(sum(drug_charge, na.rm = T) == n_charges, TRUE, FALSE),
+    charges_list = paste(count_as_filed, collapse = "; ")
+  ) |>
+  ungroup() |>
+  mutate(
+    file_month = floor_date(file_date, "months")
+  )
+
