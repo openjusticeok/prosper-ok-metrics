b632b50500eebaa0465fc74b406d6a71efc9220b
polina <prozhkova@uchicago.edu>
2023-06-30 17:13:35 -0700

    Update ocjr-hb1795.R

--- R/ocjr-hb1795.R
+++ R/ocjr-hb1795.R
@@ -11,23 +11,44 @@ library(lubridate)
 # ojo_tbl("minute") |> 
 # all our filters for dl susp |> filter(case_id %in% !!cases_of_interest$id)
 
-#relevant strings
+# DPS violation codes: https://oklahoma.gov/content/dam/ok/en/dps/VCB%20February%202022%20w%20corrections.pdf
 drugs <- "CDS|C\\.D\\.S|DRUG|OXY|HUFF|AMPHET|ZOLOL|ZOLAM|HYDROC|CODEIN|PRECURS|XANAX|MORPH|METERDI|ZEPAM|LORAZ|VALIU|EPHED|SUB|COCA|PSEUDO| CS|CS | CD|CD |PRESCRIP|NARC|METH|C\\.D\\.|HEROIN|ANHYD|AMMONIA|OPIUM|LORTAB|PARAPHERNALIA|MARIJUANA|MARIHUANA|MJ"
-count_exclude <- "DISTRIBUTE|INTENT"
+dps_codes <- "(?i) DRIVING|DI0|DI1|DI1M|DI2|DI2D|DI2DM|DI3|DI5|DI6|DI7|DI8|DL8|DR6|DR3|DR3MV"
+drug_exclude <- "DISTRIBUTE|INTENT|MANUFACTURE|DISPENSE"
+vehicle_related <- "VEHICLE"
+# 47 O.S. 2011|
 dps_related <- "(DPS|DEPARTMENT OF PUBLIC SAFETY|D\\.P\\.S)"
 exclude <- "(WITHDRAW|ERROR|RETURN|UNABLE|REVOKE|LIFT|RECALL|NOT PROCESSED|PENDING|SUSPENSION RELEASE|DEFENDANT APPEARS)"
-
-# Pulling every case with at least one misdemeanor charge
 reporting_start_date <- ymd("2022-01-01")
 reporting_end_date <- ymd("2023-04-01")
 
+# Pulling every case with at least one misdemeanor drug charge
+# 17269
 case_m <- ojo_crim_cases(case_types = "CM",
                      file_years = 2022:2023) |> 
-  collect()
+  filter(date_filed >= reporting_start_date, 
+         date_filed < reporting_end_date, 
+         str_detect(count_as_filed, drugs),
+         #str_detect(count_as_filed, vehicle_related),
+         !str_detect(count_as_filed, drug_exclude)
+         ) |> 
+  collect() #|> 
+  filter(str_detect(count_as_filed, dps_codes))
+  
 
-cm_ids <- case_m |>
-  pull(id)
+# look for cases that have vehicle mentioned in count_as_filed ?
+list_counts <- case_m |> 
+  group_by(count_as_filed) |> 
+  count()
 
+#12119
+# Style question: is it better to use pull() in place of $?
+#cm_ids <- unique(case_m$id) 
+cm_ids <- case_m |> 
+  pull(id) |> 
+  unique()
+
+#116067
 min <- ojo_tbl("minute") |> 
   filter(case_id %in% cm_ids,     
          date >= reporting_start_date,
@@ -39,15 +60,24 @@ min <- ojo_tbl("minute") |>
          description,
          count,
          amount) |> 
-  filter(
-    str_detect(description, "SUSPENSION|SUSPENDED|SUSPEND"))|> 
+  filter(code %in% c("ABST", "NOSPSe", "NOWS", "NOSRT", "TEXT", "CNOTE",
+                     "NOSUS", "NO", "RULE8", "STAY", "WRCI"),
+         #str_detect(description, "SUSPENSION"),
+         !str_detect(description, "(WITHDRAW|ERROR|RETURN|UNABLE|LIFT|NOT PROCESSED)")
+         ) |> 
   collect()
+
+#min_ids <- unique(min$case_id)
+min_ids <- min |> 
+  pull(case_id) |> 
+  unique()
 
 join_misd_min <- min |> 
   left_join(
     case_m, 
-    by = c("case_id" = "id"), 
-    relationship = "many-to-many"
+    by = c("case_id" = "id")
+    # , 
+    # relationship = "many-to-many"
   )
 
 min |> 
@@ -56,10 +86,13 @@ min |>
   )
 
 # Shortcut?
+# Question: why do rows repeat?
 cm <- ojo_crim_cases(case_types = "CM", 
                      file_years = 2022:2023) |> 
-  ojo_add_minutes() |> 
-  collect()
+  ojo_add_minutes(case_id %in% cm_ids) |> 
+  collect() #|> 
+  #distinct(case_id)
+
 
 # readr::write_csv(cm, here("data/misdemeanor_local.csv"))
 # local_cm_data <- read_csv(here("data", "misdemeanor_local.csv")) |>
@@ -92,7 +125,7 @@ test <- drug_cm |> filter(
 # code from existing repos below 
 ################ 
 
-# useful regex
+# regex from Brancen's "Tulsa County Driver's License Suspensions" report
 drug_charge = if_else(grepl(drugs, count_as_filed), TRUE, FALSE)
 
 filter(min_code %in% c("ABST", "NOSPSe", "NOWS", "NOSRT", "TEXT",
@@ -100,6 +133,24 @@ filter(min_code %in% c("ABST", "NOSPSe", "NOWS", "NOSRT", "TEXT",
        str_detect(min_desc, "SUSPENSION"),
        !str_detect(min_desc, "(WITHDRAW|ERROR|RETURN|REVOKE|UNABLE|LIFT|NOT PROCESSED|PENDING|SUSPENSION RELEASE|DEFENDANT APPEARS)"))
 
-
+# from Andrew's dl-revocation code
+# Grouping / summarizing the charges by case
+cm_cases <- cm_charges |>
+  group_by(case_number, district) |>
+  summarize(
+    file_date = unique(date_filed),
+    file_year = unique(file_year),
+    quarter_fy = unique(quarter_fy),
+    quarter_fy_start_date = unique(quarter_fy_start_date),
+    pre_november = if_else(sum(pre_november, na.rm = T) > 0, TRUE, FALSE),
+    n_charges = n(),
+    drug_charge_present = if_else(sum(drug_charge, na.rm = T) > 0, TRUE, FALSE),
+    all_drugs = if_else(sum(drug_charge, na.rm = T) == n_charges, TRUE, FALSE),
+    charges_list = paste(count_as_filed, collapse = "; ")
+  ) |>
+  ungroup() |>
+  mutate(
+    file_month = floor_date(file_date, "months")
+  )
 
 
