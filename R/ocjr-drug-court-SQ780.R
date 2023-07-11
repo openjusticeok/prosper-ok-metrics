fc5219a30cab948765924c9f2b76ceeeeadcc257
polina <prozhkova@uchicago.edu>
2023-07-11 09:36:31 -0700

    Create ocjr-drug-court-SQ780.R

---
+++ R/ocjr-drug-court-SQ780.R
@@ -0,0 +1,16 @@
+library(readr)
+library(stringr)
+library(readxl)
+library(tidycensus)
+library(tidyverse)
+library(here)
+library(janitor)
+library(glue)  
+library(ojoverse)
+library(lubridate)
+
+# * Number of simple possession cases charged in 2014, 2015, 2016
+#
+# * Number of simple possessions in 2021 and 2022
+# 
+# * Total number of SQ780 offenses charged in 2014, 2015, and 2016
