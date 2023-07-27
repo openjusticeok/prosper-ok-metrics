3fdc0fa04827f500cf301f1c57923d324fe4f659
polina <prozhkova@okpolicy.org>
2023-07-26 18:14:15 -0700

    Update ocjr-hb2490.R

--- R/ocjr-hb2490.R
+++ R/ocjr-hb2490.R
@@ -28,6 +28,9 @@ oscn_county_list <- c(
   "ELLIS"
 )
 
+start_date <- ymd("2001-01-01")
+end_date <- ymd("2023-01-01")
+
 data_case <- ojo_tbl("case", .con = ojodb) |>
   select(
     id,
@@ -59,8 +62,13 @@ oscn_cases <- data_case |>
 # It appears that revocation or application for revocation of a suspended sentence is not uncommon and might be worth noting. 
 # If we want to include cases that result in a revocation of the suspended sentence or want to do further analysis with that on what might happen.
 data_minutes <- ojo_tbl("minute") |>    
+<<<<<<< Updated upstream
   filter(date >= "2001-01-01",
           date < "2023-01-01") |>
+=======
+  filter(date >= start_date,
+          date < end_date) |>
+>>>>>>> Stashed changes
    select(id,
           case_id,
           date,
@@ -68,9 +76,15 @@ data_minutes <- ojo_tbl("minute") |>
           description,
           count,
           amount) |>
+<<<<<<< Updated upstream
   filter(str_detect(description, "(?i)suspended sentence"), 
          !str_detect(description, "(?i)\\brevok(e|ed|ing)\\b")
          ) |>
+=======
+#   filter(str_detect(description, "(?i)suspended")
+# !str_detect(description, min_desc_exclude)
+# ) |>
+>>>>>>> Stashed changes
   ojo_collect()
 
 # check for case_id's that are in 
