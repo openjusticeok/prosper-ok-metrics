2b82d51c4aa026ea1b96c121ee2d2087378a29fc
polina <prozhkova@okpolicy.org>
2023-11-03 15:24:13 -0400

    Update num-defendants-and-ids.R

--- R/num-defendants-and-ids.R
+++ R/num-defendants-and-ids.R
@@ -9,15 +9,14 @@ library(ojodb)
 library(gt)
 library(tidyr)
 library(purrr)
-theme_set(theme_bw(base_family = "Roboto") %+replace% ojo_theme())
 
 # Number of defendants and number of unique individuals for 2022 CM + CF
-ojo_connect()
+ojodb <- ojo_connect()
 
 start_date <- ymd("2022-01-01")
 end_date <- ymd("2023-01-01")
 
-data_cases <- ojo_tbl("case") |>
+data_cases <- ojo_tbl("case", .con = ojodb) |>
   select(
     id,
     case_number,
@@ -34,4 +33,32 @@ data_cases <- ojo_tbl("case") |>
     date_filed >= start_date,
     date_filed < end_date
   ) |>
+  left_join(
+    ojo_tbl("count", .con = ojodb),
+    by = c("id" = "case_id"),
+    suffix = c("", "_count")
+  ) |>
   ojo_collect()
+
+unique_case_id_list <- data_cases |>
+  distinct(id)
+
+# number of unique id's for 2022 CM + CF
+unique_case_id_list |>
+  count()
+
+
+parties <- data_cases |>
+  left_join(
+    ojo_tbl("party", .con = ojodb) |>
+      filter(
+        role == "Defendant",
+        case_id %in% !!data_cases$id
+      ) |>
+      distinct() |>
+      ojo_collect(),
+    by = c("party" = "id")
+  )
+
+# test <- data_cases |>
+#   distinct(id, .keep_all = TRUE)
