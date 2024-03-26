bbf454c7944be07e393df04a351fefc8b4254602
polina <prozhkova@okpolicy.org>
2024-03-26 12:12:56 -0400

    updated oja data
    and correcting for unique referrals

--- R/ocjr-hb3205.R
+++ R/ocjr-hb3205.R
@@ -67,7 +67,7 @@ family_estimate_state <- get_acs(geography = "state",
                            year = 2021,
                            cache = TRUE)
 
-# OJA data with monthly referrals 2018-2022
+# data shared in 2023: OJA data with monthly referrals 2018-2022
 referrals <- read_excel(here("data", "YoungOffenders.xlsx"), sheet = "Referrals") |>
   janitor::clean_names() |>
   rename(
@@ -167,3 +167,244 @@ county_referrals_quarter |>
     total_family_impact_estimate = sum(impact_estimate, na.rm = TRUE) |>
       round()
   )
+
+# ______________________________________________________________________________
+# old code from qmd report - 2023
+county_referrals_quarter <- referrals |>
+  group_by(
+    county,
+    month,
+    quarter,
+    fiscal_year,
+    fiscal_quarter
+  ) |>
+  count() |>
+  rename(
+    num_referred = n
+  ) |>
+  ungroup() |>
+  left_join(
+    hh_estimate |>
+      select(
+        county,
+        household_size = estimate
+      ),
+    by = "county"
+  ) |>
+  left_join(
+    family_estimate |>
+      select(
+        county,
+        family_size = estimate
+      ),
+    by = "county"
+  ) |>
+  mutate(
+    household_impact_estimate = num_referred * household_size,
+    family_impact_estimate = num_referred * family_size
+  )
+#
+# gt(county_referrals_quarter) |>
+#   cols_label(
+#     county = "County",
+#     month = "Month",
+#     quarter = "Quarter Start",
+#     fiscal_year = "Fiscal Year",
+#     fiscal_quarter = "Fiscal Quarter",
+#     num_referred = "Referrals",
+#     household_size = "Average Household Size",
+#     household_impact_estimate = "Impacted Oklahomans (Low)",
+#     family_size = "Average Family Size",
+#     family_impact_estimate = "Impacted Oklahomans (High)"
+#   ) |>
+#   fmt_number(
+#     columns = ends_with("_estimate"),
+#     decimals = 0
+#   ) |>
+#   tab_footnote(
+#     footnote = "American Community Survey 5-Year Data (2009-2021), Households and Families (S1101)",
+#     locations = cells_column_labels(
+#       columns = c(household_size, family_size)
+#     )
+#   ) |>
+#   tab_source_note(
+#     county_referrals_quarter |>
+#       download_this(
+#         output_name = "oja_referrals_county_quarterly",
+#         output_extension = ".csv"
+#       )
+#   ) |>
+#   tab_options(
+#     ihtml.active = TRUE,
+#     ihtml.use_filters = TRUE,
+#     ihtml.use_highlight = TRUE,
+#     ihtml.use_text_wrapping = TRUE,
+#     quarto.use_bootstrap = TRUE
+#   )
+
+county_referrals_quarter |>
+  filter(
+    quarter >= "2022-10-01"
+  ) |>
+  group_by(
+    fiscal_year,
+    fiscal_quarter,
+    quarter
+  ) |>
+  summarise(
+    total_referred = sum(num_referred, na.rm = TRUE),
+    household_impact_estimate = sum(household_impact_estimate, na.rm = TRUE),
+    family_impact_estimate = sum(family_impact_estimate, na.rm = TRUE)
+  ) |>
+  ungroup() |>
+  gt() |>
+  grand_summary_rows(
+    columns = c(
+      total_referred,
+      household_impact_estimate,
+      family_impact_estimate
+    ),
+    fns = list(
+      label = "Total",
+      id = "total",
+      fn = "sum"
+    ),
+    fmt = ~ fmt_number(., decimals = 0)
+  ) |>
+  cols_label(
+    quarter = "Quarter Start",
+    fiscal_year = "Fiscal Year",
+    fiscal_quarter = "Fiscal Quarter",
+    total_referred = "Referrals",
+    household_impact_estimate = "Household Impact",
+    family_impact_estimate = "Family Impact"
+  ) |>
+  fmt_number(
+    columns = c("total_referred", "household_impact_estimate", "family_impact_estimate"),
+    decimals = 0
+  )
+
+#_______________________________________________________________________________
+## Correcting overestimate and assuming that each referral id represents one person (this is not always true).
+# The previous code treated each row of data as a unique individual not taking into account that each
+#referral id represents a "case" and each row is an individual charge.
+
+unique_county_referrals_quarter <- referrals |>
+  distinct(referral_id, .keep_all = TRUE) |>
+  group_by(
+    county,
+    month,
+    quarter,
+    fiscal_year,
+    fiscal_quarter
+  ) |>
+  count() |>
+  rename(
+    num_referred = n
+  ) |>
+  ungroup() |>
+  left_join(
+    hh_estimate |>
+      select(
+        county,
+        household_size = estimate
+      ),
+    by = "county"
+  ) |>
+  left_join(
+    family_estimate |>
+      select(
+        county,
+        family_size = estimate
+      ),
+    by = "county"
+  ) |>
+  mutate(
+    household_impact_estimate = num_referred * household_size,
+    family_impact_estimate = num_referred * family_size
+  )
+
+# gt(unique_county_referrals_quarter) |>
+#   cols_label(
+#     county = "County",
+#     month = "Month",
+#     quarter = "Quarter Start",
+#     fiscal_year = "Fiscal Year",
+#     fiscal_quarter = "Fiscal Quarter",
+#     num_referred = "Referrals",
+#     household_size = "Average Household Size",
+#     household_impact_estimate = "Impacted Oklahomans (Low)",
+#     family_size = "Average Family Size",
+#     family_impact_estimate = "Impacted Oklahomans (High)"
+#   ) |>
+#   fmt_number(
+#     columns = ends_with("_estimate"),
+#     decimals = 0
+#   ) |>
+#   tab_footnote(
+#     footnote = "American Community Survey 5-Year Data (2009-2021), Households and Families (S1101)",
+#     locations = cells_column_labels(
+#       columns = c(household_size, family_size)
+#     )
+#   ) |>
+#   tab_source_note(
+#     county_referrals_quarter |>
+#       download_this(
+#         output_name = "oja_referrals_county_quarterly",
+#         output_extension = ".csv"
+#       )
+#   ) |>
+#   tab_options(
+#     ihtml.active = TRUE,
+#     ihtml.use_filters = TRUE,
+#     ihtml.use_highlight = TRUE,
+#     ihtml.use_text_wrapping = TRUE,
+#     quarto.use_bootstrap = TRUE
+#   )
+
+## Testing unique referral counts
+unique_county_referrals_quarter |>
+  filter(
+    quarter >= "2022-10-01"
+  ) |>
+  group_by(
+    fiscal_year,
+    fiscal_quarter,
+    quarter
+  ) |>
+  summarise(
+    total_referred = sum(num_referred, na.rm = TRUE),
+    household_impact_estimate = sum(household_impact_estimate, na.rm = TRUE),
+    family_impact_estimate = sum(family_impact_estimate, na.rm = TRUE)
+  ) |>
+  ungroup() |>
+  gt() |>
+  grand_summary_rows(
+    columns = c(
+      total_referred,
+      household_impact_estimate,
+      family_impact_estimate
+    ),
+    fns = list(
+      label = "Total",
+      id = "total",
+      fn = "sum"
+    ),
+    fmt = ~ fmt_number(., decimals = 0)
+  ) |>
+  cols_label(
+    quarter = "Quarter Start",
+    fiscal_year = "Fiscal Year",
+    fiscal_quarter = "Fiscal Quarter",
+    total_referred = "Referrals",
+    household_impact_estimate = "Household Impact",
+    family_impact_estimate = "Family Impact"
+  ) |>
+  fmt_number(
+    columns = c("total_referred", "household_impact_estimate", "family_impact_estimate"),
+    decimals = 0
+  )
+
+#_______________________________________________________________________________
+
+
