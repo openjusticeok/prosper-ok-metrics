9ee70d1d2a270a6eaa3d8f696cb16d1f96515eaa
Anthony Flores <anthonyfloresokc@gmail.com>
2025-12-01 11:57:38 -0600

    style: Format function parameters across jail and prison report renderers

--- R/render_report_jail.R
+++ R/render_report_jail.R
@@ -1,4 +1,9 @@
-render_jail_report <- function(analysis_results, figure_outputs, report_source = pipeline_report_path("jail"), execute = TRUE) {
+render_jail_report <- function(
+  analysis_results,
+  figure_outputs,
+  report_source = pipeline_report_path("jail"),
+  execute = TRUE
+) {
   render_report(
     "jail",
     analysis_results,
