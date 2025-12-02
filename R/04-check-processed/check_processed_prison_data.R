df6d98c96ffa8c3f9a9aa606459c80d755e1f5cc
Anthony Flores <anthonyfloresokc@gmail.com>
2025-12-01 19:32:15 -0600

    feat(check): Add prison data validation functions and checks

--- R/04-check-processed/check_processed_prison_data.R
+++ R/04-check-processed/check_processed_prison_data.R
@@ -1,3 +1,46 @@
+# TODO: All of this is scaffold code and needs to be implemented properly.
+summarise_missing <- function(data, field) {
+  data |>
+    dplyr::summarise(
+      missing = sum(is.na({{ field }})),
+      total = dplyr::n()
+    )
+}
+
+summarise_date_range <- function(data, date_field) {
+  out <- data |>
+    dplyr::summarise(
+      min_date = min({{ date_field }}, na.rm = TRUE),
+      max_date = max({{ date_field }}, na.rm = TRUE),
+      total = dplyr::n(),
+      n_people = dplyr::n_distinct(.data$doc_num)
+    )
+
+  out |>
+    dplyr::mutate(
+      min_date = dplyr::if_else(is.infinite(as.numeric(.data$min_date)), as.Date(NA), .data$min_date),
+      max_date = dplyr::if_else(is.infinite(as.numeric(.data$max_date)), as.Date(NA), .data$max_date)
+    )
+}
+
 check_prison_processed <- function(processed_data = prison_processed_data) {
-  processed_data
+  doc_population <- processed_data$doc_population
+
+  missing_county <- summarise_missing(doc_population, .data$most_recent_sentencing_county)
+  missing_date <- summarise_missing(doc_population, .data$most_recent_sentencing_date)
+  date_range <- summarise_date_range(doc_population, .data$most_recent_sentencing_date)
+
+  tibble::tibble(
+    check = c(
+      "most_recent_sentencing_county_missing",
+      "most_recent_sentencing_date_missing",
+      "most_recent_sentencing_date_range"
+    ),
+    status = c(
+      ifelse(missing_county$missing == 0, "pass", "warning"),
+      ifelse(missing_date$missing == 0, "pass", "warning"),
+      "info"
+    ),
+    details = list(missing_county, missing_date, date_range)
+  )
 }
