5a99bd09bde03bcb0eaa432448d78ffbcf8a9097
Anthony Flores <anthonyfloresokc@gmail.com>
2025-12-02 08:24:29 -0600

    feat(process): Parse date columns as needed (dates, times, datetimes), add helpers

--- R/03-process/process_ingested_jail_data.R
+++ R/03-process/process_ingested_jail_data.R
@@ -1,3 +1,34 @@
+### Helper functions
+standardize_sex_gender <- function(data, col = "gender") {
+  values <- data[[col]]
+
+  dplyr::case_when(
+    stringr::str_detect(values, "(?i)^f(emale)?$") ~ "Female",
+    stringr::str_detect(values, "(?i)^m(ale)?$") ~ "Male",
+    TRUE ~ "Other/Unknown Gender"
+  )
+}
+
+standardize_race_ethnicity <- function(data, col = "race") {
+  values <- data[[col]]
+
+  # WARNING: Recodes for A, H, I, N, O, P, and U are guessed based on
+  # other data sources. They have not been corroborated by jail staff.
+  dplyr::case_when(
+    stringr::str_detect(values, "(?i)^i$|^n$") ~ "American Indian or Alaska Native",
+    stringr::str_detect(values, "(?i)^a$|^p$") ~ "Asian American and Pacific Islander",
+    stringr::str_detect(values, "(?i)^b$") ~ "Black",
+    stringr::str_detect(values, "(?i)^h$") ~ "Hispanic/Latino",
+    stringr::str_detect(values, "(?i)^w$") ~ "White",
+    stringr::str_detect(values, "(?i)^o$") ~ "Other Race/Ethnicity",
+    stringr::str_detect(values, "(?i)^u$") ~ "Unknown Race/Ethnicity",
+    TRUE ~ "Unknown Race/Ethnicity"
+  )
+}
+
+
+
+### Main processing function
 process_ingested_jail_data <- function(ingested_checks = jail_ingested_checks) {
   ingested_data <- ingested_checks$ingested_data
 
@@ -5,22 +36,24 @@ process_ingested_jail_data <- function(ingested_checks = jail_ingested_checks) {
   # We clean here instead of downstream so that future joins can treat JDI rows
   # the same as Asemio or OK Policy scrapes.
   # TODO: feat(process): Normalize column names
-  # TODO: feat(process): Parse date columns as needed
-  # TODO: feat(process): Clean race columns as needed
-  # TODO: feat(process): Clean gender columns as needed
+  # TODO: feat(process): Parse date columns as needed (dates, times, datetimes)
+    # TODO: fix(process) convert Period types to time types
+    # NOTE: It's easier when I make the datetime and derive date and time from that
+  # TODO: feat(process): Standardize race columns as needed
+  # TODO: feat(process): Standardize gender columns as needed
   # TODO: feat(process): Bind data from multiple sources as needed
   # TODO: feat(process): Summarize and generate derived columns as needed
   # TODO: chore(process): refactor processing functions to use helper functions where appropriate
   # TODO: chore(process): Move code to explore if interesting but not needed in processing
   # TODO: feat(process): Determine timezone of each data source
   # TODO: Pull out some helper functions for repeated processing tasks
-    # The helper functions below enforce consistent schemas, fill in missing
-    # columns, and coerce date/time fields from the Jail Data Initiative CSVs.
-    # E.g., parsing date times, normalizing names, etc.
-    # Only do this if it improves readability and confidence in uniform processing
+  # The helper functions below enforce consistent schemas, fill in missing
+  # columns, and coerce date/time fields from the Jail Data Initiative CSVs.
+  # E.g., parsing date times, normalizing names, etc.
+  # Only do this if it improves readability and confidence in uniform processing
 
-  # Jail Data Initiative Data Processing
-  jail_data_initiative_people <- ingested_data$jail_data_initiative$people |>
+  # Process Jail Data Initiative Scraped Data
+  jail_data_initiative_bookings_with_inmate_info <- ingested_data$jail_data_initiative$people |>
     janitor::clean_names() |>
     dplyr::rename(
       birth_date = "dob",
@@ -28,46 +61,42 @@ process_ingested_jail_data <- function(ingested_checks = jail_ingested_checks) {
     ) |>
     dplyr::mutate(
       source = "Jail Data Initiative",
+      # Direct PII
+      # Indirect PII
+      age = as.integer(.data$age),
+      age_standardized = as.double(.data$age_standardized),
+      # Dates
       arrest_booking_date = lubridate::mdy(.data$arrest_booking_date),
       # TODO: Confirm time zone by comparing to other known timezone
-      arrest_booking_time = lubridate::hm(.data$arrest_booking_time),
+      arrest_booking_time = hms::as_hms(lubridate::parse_date_time(.data$arrest_booking_time, orders = c("H:M"))),
       arrest_booking_datetime = as.POSIXct(
         .data$arrest_booking_date + .data$arrest_booking_time,
         tz = "America/Chicago"
       ),
       arrest_date = lubridate::ymd(.data$arrest_date),
-      arrest_time = lubridate::hm(.data$arrest_time),
+      arrest_time = hms::as_hms(lubridate::parse_date_time(.data$arrest_time, orders = c("H:M"))),
       arrest_datetime = as.POSIXct(
         .data$arrest_date + .data$arrest_time,
         tz = "America/Chicago"
       ),
       birth_date = lubridate::ymd(.data$birth_date),
       booking_date = lubridate::ymd(.data$booking_date),
-      booking_time = lubridate::hm(.data$booking_time),
+      booking_time = hms::as_hms(lubridate::parse_date_time(.data$booking_time, orders = c("H:M"))),
       released_date = lubridate::ymd(.data$released_date),
-      released_time = lubridate::hm(.data$released_time),
+      released_time = hms::as_hms(lubridate::parse_date_time(.data$released_time, orders = c("H:M"))),
       released_datetime = as.POSIXct(
         .data$released_date + .data$released_time,
         tz = "America/Chicago"
       ),
       release_date = lubridate::ymd(.data$release_date),
-      release_time = lubridate::hm(.data$release_time),
+      release_time = hms::as_hms(lubridate::parse_date_time(.data$release_time, orders = c("H:M"))),
       release_datetime = as.POSIXct(
         .data$release_date + .data$release_time,
         tz = "America/Chicago"
       ),
       meta_first_seen = lubridate::ymd_hms(.data$meta_first_seen, tz = "America/Chicago"),
       meta_last_seen = lubridate::ymd_hms(.data$meta_last_seen, tz = "America/Chicago"),
-      meta_scrape_date = lubridate::ymd(.data$meta_scrape_date, quiet = TRUE), # Loud because of NAs
-      age = as.integer(.data$age),
-      age_standardized = as.double(.data$age_standardized)
-    ) |>
-    dplyr::mutate(
-      jacket_number_type = dplyr::case_when(
-        is.na(.data$jacket_number) & !is.na(.data$jacket_number_old) ~ "old",
-        !is.na(.data$jacket_number) ~ "new",
-        TRUE ~ NA_character_
-      )
+      meta_scrape_date = lubridate::ymd(.data$meta_scrape_date, quiet = TRUE) # Loud because of NAs
     )
 
   jail_data_initiative_charges <- ingested_data$jail_data_initiative$charges |>
@@ -79,20 +108,20 @@ process_ingested_jail_data <- function(ingested_checks = jail_ingested_checks) {
       case_year = stringr::str_extract(.data$case_number, "^[A-Z]+-(\\d+)-\\d+", group = 1),
       # TODO: If case_year is unreliable, case_number_id is likely unreliable as
       # well, but potentially even harder to notice errors (needs matching with OSCN data)
-      case_number = stringr::str_extract(.data$case_number, "^[A-Z]+-\\d+-(\\d+)", group = 1),
+      case_index = stringr::str_extract(.data$case_number, "^[A-Z]+-\\d+-(\\d+)", group = 1),
       court_date = lubridate::ymd(.data$court_date),
       bond_amount = as.double(.data$bond_amount),
       bail_amount = as.double(.data$bail_amount),
       bail_set_date = lubridate::ymd(.data$bail_set_date),
-      bail_set_time = lubridate::hm(.data$bail_set_time),
+      bail_set_time = hms::as_hms(lubridate::parse_date_time(.data$bail_set_time, orders = c("H:M"))),
       # TODO: Confirm time zone by comparing to other known timezone
       bail_set_datetime = as.POSIXct(
         .data$bail_set_date + .data$bail_set_time,
         tz = "America/Chicago"
       )
-    ) |>
+    )
 
-  # Asemio Scraper Data Processing
+  # Process Asemio Scraper Data
   asemio_scraped_bookings <- ingested_data$asemio$bookings |>
     dplyr::rename(
       jacket_number = dlm,
@@ -101,28 +130,35 @@ process_ingested_jail_data <- function(ingested_checks = jail_ingested_checks) {
     ) |>
     dplyr::mutate(
       source = "Asemio Scraper",
+      time = hms::as_hms(lubridate::parse_date_time(time, orders = c("IM p"))),
+      booking_time = hms::as_hms(lubridate::parse_date_time(booking_time, orders = c("IM p"))),
+      release_time = hms::as_hms(lubridate::parse_date_time(release_time, orders = c("IM p"))),
       created_at_date = as.Date(created_at),
       updated_at_date = as.Date(updated_at),
+      created_at_time = hms::parse_hms(stringr::str_sub(created_at, 12, 19)),
+      updated_at_time = hms::parse_hms(stringr::str_sub(updated_at, 12, 19)),
       jacket_number = as.character(.data$jacket_number)
     )
 
   asemio_scraped_inmates <- ingested_data$asemio$inmates |>
-    dplyr::mutate(
-      source = "Asemio Scraper",
-      created_at_date = as.Date(created_at),
-      updated_at_date = as.Date(updated_at)
-    ) |>
     dplyr::rename(
       jacket_number = dlm,
       given_name = first_name,
       surname = last_name,
     ) |>
-    # TODO: Decide how to normalize asemio & okpolicy name columns
-    # asemio data has no middle name
-    # Create full name column
     dplyr::mutate(
+      source = "Asemio Scraper",
+      # Direct PII
       jacket_number = as.character(.data$jacket_number),
-      full_name = paste(.data$given_name, .data$surname)
+      # TODO: Decide how to normalize asemio & okpolicy full name columns
+      # Asemio data has no middle name, so just concatenate given_name and surname
+      full_name = paste(.data$given_name, .data$surname),
+      # Indirect PII
+      sex_gender_standardized = standardize_sex_gender(data = ., col = "gender"),
+      race_ethnicity_standardized = standardize_race_ethnicity(data = ., col = "race"),
+      # Other Dates
+      created_at_date = as.Date(created_at),
+      updated_at_date = as.Date(updated_at)
     )
 
 
@@ -141,28 +177,53 @@ process_ingested_jail_data <- function(ingested_checks = jail_ingested_checks) {
     )
 
 
-  # OK Policy Scraper Data Processing
-  okpolicy_scraped_bookings <- ingested_data$okpolicy$bookings |>
+  # Process OK Policy Scraper Data
+  okpolicy_scraped_bookings_with_inmate_info <- ingested_data$okpolicy$bookings |>
+    dplyr::rename(
+      created_at_date = created_at,
+      updated_at_datetime = updated_at
+    ) |>
     dplyr::mutate(
+      source = "OK Policy Scraper",
+      # Direct PII
       full_name = paste(.data$given_name, .data$middle_name, .data$surname),
+      # Indirect PII
+      gender = stringr::str_to_title(.data$gender),
+      race = stringr::str_to_title(.data$race),
       birth_date = lubridate::mdy(.data$birth_date),
-      # Booking date is really booking datetime
+      # Other Dates
       booking_datetime = lubridate::ymd_hms(
         .data$booking_date,
         # TODO: We actually need to confirm the timezone of the booking date
         tz = "America/Chicago",
         quiet = TRUE
       ),
-      booking_date = as.Date(.data$booking_datetime),
-      source = "OK Policy Scraper",
-      gender = stringr::str_to_title(.data$gender),
-      race = stringr::str_to_title(.data$race),
-      created_at_date = as.Date(created_at),
-      updated_at_date = as.Date(updated_at)
+      booking_date = as.Date(.data$booking_datetime), # Booking date is really booking datetime
+      booking_time = hms::as_hms(.data$booking_datetime),
+      updated_at_date = as.Date(updated_at_datetime),
+      updated_at_time = hms::as_hms(updated_at_datetime)
     )
 
+  asemio_scraped_inmates |>
+    dplyr::count(race, race_ethnicity_standardized)
+  asemio_scraped_inmates |>
+    dplyr::count(gender, sex_gender_standardized)
+  jail_data_initiative_bookings_with_inmate_info |>
+    dplyr::count(race, race_ethnicity_standardized)
+  jail_data_initiative_bookings_with_inmate_info |>
+    dplyr::count(sex, sex_gender_standardized)
+
   okpolicy_scraped_charges <- ingested_data$okpolicy$charges |>
+    dplyr::rename(
+      charge = statute_title,
+      charge_description = statute_description,
+      arresting_agency = charging_agency_abbreviation,
+      updated_at_datetime = updated_at,
+      # WARNING: This will break if I change created_at to a datetime later.
+      created_at_date = created_at
+    ) |>
     dplyr::mutate(
+      source = "OK Policy Scraper",
       bail_set_datetime = lubridate::ymd_hms(
         # TODO: We actually need to confirm the timezone of the booking date
         .data$bail_set_date,
@@ -170,18 +231,14 @@ process_ingested_jail_data <- function(ingested_checks = jail_ingested_checks) {
         quiet = TRUE
       ),
       bail_set_date = as.Date(.data$bail_set_datetime),
-      created_at_date = as.Date(created_at),
-      updated_at_date = as.Date(updated_at)
-    ) |>
-    dplyr::rename(
-      charge = statute_title,
-      charge_description = statute_description,
-      arresting_agency_abbreviation = charging_agency_abbreviation
+      created_at_date = as.Date(created_at_date),
+      updated_at_date = as.Date(updated_at_datetime),
+      updated_at_time = hms::as_hms(updated_at_datetime),
     )
 
 
-  ## Joining data
 
+  ### Joining data
   # Join inmate info to bookings for Asemio data
   asemio_scraped_bookings_with_inmate_info <- asemio_scraped_bookings |>
     dplyr::left_join(
@@ -192,7 +249,7 @@ process_ingested_jail_data <- function(ingested_checks = jail_ingested_checks) {
 
   # Join booking info to charges for OK Policy data
   # This is needed to filter bookings with certain hold charges
-  okpolicy_scraped_charges_with_bookings_info <- okpolicy_scraped_bookings |>
+  okpolicy_scraped_charges_with_bookings_info <- okpolicy_scraped_bookings_with_inmate_info |>
     dplyr::left_join(
       okpolicy_scraped_charges,
       by = c("id" = "booking_id"),
@@ -200,7 +257,7 @@ process_ingested_jail_data <- function(ingested_checks = jail_ingested_checks) {
     )
 
 
-  # Filter out hold charges unless for City of Tulsa or Electronic Monitoring Required
+  ### Filter out hold charges unless for City of Tulsa or Electronic Monitoring Required
   # TODO: Finer replacement with what the charge actually is rather than
   # the hold category. I.e., Merging typos rather than "Tribal Hold"
   # Hold Categories
@@ -297,7 +354,8 @@ process_ingested_jail_data <- function(ingested_checks = jail_ingested_checks) {
 
   # Combine bookings data from both sources
   booking_sources <- list(
-    okpolicy_scraped_bookings,
+    jail_data_initiative_bookings_with_inmate_info,
+    okpolicy_scraped_charges_with_bookings_info,
     asemio_scraped_bookings_with_inmate_info
   )
 
