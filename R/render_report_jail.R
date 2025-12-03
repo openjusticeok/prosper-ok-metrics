2ad35376a0f244b96463ab217056c46c294a46fc
Anthony Flores <anthonyfloresokc@gmail.com>
2025-12-03 09:43:12 -0600

    feat(report): Add and “internal” report that includes some additional figures we don’t share to external partners.
    - feat(report): Add parameter option for “internal”
    - fix(report): Rename private to “external” for external organization in
    all scripts.
    - Turn off the external report for dev iteration speed.

--- R/render_report_jail.R
+++ R/render_report_jail.R
@@ -2,13 +2,15 @@ render_jail_report <- function(
   analysis_results,
   figure_outputs,
   report_source = pipeline_report_path("jail"),
-  execute = TRUE
+  execute = TRUE,
+  audiences = "internal"
 ) {
   render_report(
     "jail",
     analysis_results,
     figure_outputs,
     execute = execute,
-    input = report_source
+    input = report_source,
+    audiences = audiences
   )
 }
