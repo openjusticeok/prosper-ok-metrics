e6c7d75bca0aa3d9cbf1abe04b44e6ba4db50950
Anthony Flores <anthonyfloresokc@gmail.com>
2025-12-04 13:45:22 -0600

    chore(prison): Remove blank line and add TODO comments for metrics refactoring

--- R/06-visualize/visualize_prison_figures.R
+++ R/06-visualize/visualize_prison_figures.R
@@ -1,5 +1,4 @@
 visualize_prison_figures <- function(analysis_results = prison_analysis_results) {
-
   plot_sentences_doc_admin_year_total_by_county <- placeholder_ggplot()
   plot_releases_vera_admin_year_total_by_county <- placeholder_ggplot()
   plot_population_doc_admin_yoy_by_gender_county <- placeholder_ggplot()
