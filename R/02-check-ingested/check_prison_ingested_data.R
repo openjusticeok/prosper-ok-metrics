a9060f1524264a3ac8481140c0b35c8d152dea97
Anthony Flores <anthonyfloresokc@gmail.com>
2026-02-16 19:19:18 -0600

    fix(prison): Add cross-validation checks for doc_num consistency between profile and sentence data

--- R/02-check-ingested/check_prison_ingested_data.R
+++ R/02-check-ingested/check_prison_ingested_data.R
@@ -1,5 +1,4 @@
 check_prison_ingested <- function(ingested_data = prison_ingested_data) {
-
   doc_ingested_data <- ingested_data$doc
 
   # WARNING: These are just placeholder checks. Real checks needed.
@@ -13,6 +12,12 @@ check_prison_ingested <- function(ingested_data = prison_ingested_data) {
   ) |>
     pointblank::col_exists(pointblank::vars(doc_num, facility, status, birth_date)) |>
     pointblank::col_vals_not_null(pointblank::vars(doc_num)) |>
+    # Check that all doc_num's in the profile data appear in the sentence data
+    # I.e. profile doc_num's ⊆ sentence doc_num's
+    pointblank::col_vals_in_set(
+      pointblank::vars(doc_num),
+      set = unique(doc_ingested_data$sentence_data$doc_num)
+    ) |>
     pointblank::interrogate()
 
   sentence_agent <- pointblank::create_agent(
@@ -22,6 +27,14 @@ check_prison_ingested <- function(ingested_data = prison_ingested_data) {
   ) |>
     pointblank::col_exists(pointblank::vars(sentence_id, doc_num, sentencing_county, js_date)) |>
     pointblank::col_is_date(pointblank::vars(js_date), actions = warn_only) |>
+    pointblank::col_vals_not_null(pointblank::vars(sentence_id)) |>
+    pointblank::col_vals_not_null(pointblank::vars(doc_num)) |>
+    # Check that all doc_num's in the sentence data appear in the profile data
+    # I.e. sentence doc_num's ⊆ profile doc_num's
+    pointblank::col_vals_in_set(
+      pointblank::vars(doc_num),
+      set = unique(doc_ingested_data$profile_data$doc_num)
+    ) |>
     pointblank::interrogate()
 
   offense_agent <- pointblank::create_agent(
