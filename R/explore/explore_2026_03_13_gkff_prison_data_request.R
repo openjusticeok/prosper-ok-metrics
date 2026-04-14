54795cee996a5543d10daf91bfa35ff7d2f39177
Anthony Flores <anthonyfloresokc@gmail.com>
2026-04-14 05:18:47 -0500

    feat(prison): add processing and population analysis for 2026-03-13 gkff data

--- R/explore/explore_2026_03_13_gkff_prison_data_request.R
+++ R/explore/explore_2026_03_13_gkff_prison_data_request.R
@@ -9,6 +9,8 @@ library(here)
 library(readxl)
 library(pointblank)
 library(janitor)
+library(benchCalculatePopulation)
+
 
 # Ingest
 single_drive_folder_url <- "https://drive.google.com/drive/u/0/folders/1AcwuaOHYifDDYHCHQaykAzLba1yDkHcZ"
@@ -18,18 +20,18 @@ drive_files <- googledrive::drive_ls(folder_id)
 snapshot_date <- lubridate::ymd("2026-03-13")
 input_dir <- here::here("data/input/doc/2026-03-13-gkff/")
 
-fs::dir_create(input_dir)
-
-purrr::pwalk(
-  .l = drive_files,
-  .f = function(id, name, ...) {
-    googledrive::drive_download(
-      file = id,
-      path = here::here(input_dir, name),
-      overwrite = TRUE
-    )
-  }
-)
+# fs::dir_create(input_dir)
+#
+# purrr::pwalk(
+#   .l = drive_files,
+#   .f = function(id, name, ...) {
+#     googledrive::drive_download(
+#       file = id,
+#       path = here::here(input_dir, name),
+#       overwrite = TRUE
+#     )
+#   }
+# )
 
 sentences_data <- readxl::read_xlsx(here(input_dir, "Sentences.xlsx")) |>
   janitor::clean_names()
@@ -43,6 +45,7 @@ offense_data <- readxl::read_xlsx(here(input_dir, "Offense.xlsx")) |>
   janitor::clean_names()
 alias_data <- readxl::read_xlsx(here(input_dir, "Alias.xlsx")) |>
   janitor::clean_names()
+
 
 # Check Ingested
 sentences_data |>
@@ -161,11 +164,18 @@ releases_agent <- releases_data |>
   pointblank::col_vals_not_null(facility) |>
   pointblank::interrogate()
 
-profile_data <- profile_data |>
+profile_agent <- profile_data |>
   pointblank::create_agent(
     tbl_name = "ODOC Inmate Profile",
     label = "Profile Data"
   ) |>
+  # TODO: Checking uniqueness
+  # It is unique by doc_num and admit date which is most improtant
+  # Not unique with doc_num and each of: name (especially suffix), race,
+  # facility, maybe others. Need to check this more but may not be an issue.
+  # It appears the inconsitency with race which it notable is only present when
+  # the jail backlog has an issue.
+  # Checking missingness
   pointblank::col_vals_not_null(doc_num) |>
   pointblank::col_vals_not_null(first_name) |>
   pointblank::col_vals_not_null(last_name) |>
@@ -184,4 +194,277 @@ profile_data <- profile_data |>
   pointblank::col_vals_lte(birth_date, snapshot_date) |>
   pointblank::interrogate()
 
+# TODO: Add offense and alias checks
+# TODO: Incorporate the checks Polina created
+
+
 # Process
+assessment_and_reception <- c(
+  "LEXINGTON ASSESSMENT AND RECEPTION CENTER",
+  "MABEL BASSETT ASSESSMENT & RECEPTION CENTER"
+)
+
+state_institutions <- c(
+  "ALLEN GAMBLE CORRECTIONAL CENTER",
+  "CHARLES E. (BILL) JOHNSON CORRECTIONAL CENTER",
+  "DICK CONNER CORRECTIONAL CENTER",
+  "DR. EDDIE WARRIOR CORRECTIONAL CENTER (MIN)",
+  "GREAT PLAINS CORRECTIONAL CENTER",
+  "HOWARD MCLEOD CORRECTIONAL CENTER",
+  "JACKIE BRANNON CORRECTIONAL CENTER",
+  "JAMES CRABTREE CORRECTIONAL CENTER",
+  "JESS DUNN CORRECTIONAL CENTER",
+  "JIM E. HAMILTON CORRECTIONAL CENTER",
+  "JOHN H. LILLEY CORRECTIONAL CENTER",
+  "JOSEPH HARP CORRECTIONAL CENTER",
+  "LEXINGTON CORRECTIONAL CENTER",
+  "MABEL BASSETT CORRECTIONAL CENTER",
+  "MACK ALFORD CORRECTIONAL CENTER",
+  "OKLAHOMA STATE PENITENTIARY",
+  "OKLAHOMA STATE REFORMATORY",
+  "RED ROCK CORRECTIONAL CENTER" # previously LAWTON CORRECTIONAL AND REHABILITATION FACILITY
+)
+
+private_facilities <- c("LAWTON CORRECTIONAL AND REHABILITATION FACILITY")
+
+community_centers <- c(
+  "CLARA WATERS COMMUNITY CORRECTIONS CENTER",
+  "DR. EDDIE WARRIOR CORRECTIONAL CENTER (CCC)",
+  "ENID COMMUNITY CORRECTIONS CENTER",
+  "LAWTON COMMUNITY CORRECTIONS CENTER",
+  "NORTHEAST OKLAHOMA COMMUNITY CORRECTIONS CENTER",
+  "OKLAHOMA CITY COMMUNITY CORRECTIONS CENTER",
+  "UNION CITY COMMUNITY CORRECTIONS CENTER"
+)
+
+halfway_house <- c("BRIDGEWAY HALFWAY HOUSE")
+
+interstate <- c("INTERSTATE COMPACT (OUT TO OTHER STATE) UNIT")
+
+profile_data <- profile_data |>
+  dplyr::mutate(snapshot_date = snapshot_date) |>
+  mutate(
+    birth_date = ymd(birth_date),
+    admit_date = ymd(admit_date),
+    release_date = ymd(release_date)
+  ) |>
+  mutate(
+    physical_stay = lubridate::interval(
+      start = admit_date,
+      end = if_else(is.na(release_date), snapshot_date, release_date)
+    )
+  ) |>
+  # Construct full name variable
+  dplyr::mutate(
+    # TODO: Reconsider name formatting for consistency with other datasets
+    name = stringr::str_c(
+      dplyr::if_else(!is.na(.data$last_name), stringr::str_c(" ", .data$last_name), ""),
+      dplyr::if_else(!is.na(.data$suffix), stringr::str_c(" ", .data$suffix), ""),
+      ", ",
+      dplyr::if_else(!is.na(.data$first_name), stringr::str_c(" ", .data$first_name), ""),
+      " ",
+      dplyr::if_else(!is.na(.data$middle_name), stringr::str_c(" ", .data$middle_name), ""),
+      "."
+    ),
+    # Construct snapshot_age, current_age, and age_bucket variables based on birth_date
+    snapshot_age = floor(lubridate::time_length(lubridate::interval(.data$birth_date, as.Date(.data$snapshot_date)), "years")),
+    current_age = floor(lubridate::time_length(lubridate::interval(.data$birth_date, lubridate::today("America/Chicago")), "years")),
+    current_age_date = today(), # Day the current age was calculated, which can be useful for tracking when age-based analyses were conducted
+    snapshot_age_bucket = dplyr::case_when(
+      .data$snapshot_age < 18 ~ "Under 18",
+      .data$snapshot_age >= 18 & .data$snapshot_age < 25 ~ "18-24",
+      .data$snapshot_age >= 25 & .data$snapshot_age < 35 ~ "25-34",
+      .data$snapshot_age >= 35 & .data$snapshot_age < 45 ~ "35-44",
+      .data$snapshot_age >= 45 & .data$snapshot_age < 55 ~ "45-54",
+      .data$snapshot_age >= 55 & .data$snapshot_age < 65 ~ "55-64",
+      .data$snapshot_age >= 65 ~ "65 and older",
+      TRUE ~ "Unknown"
+    ),
+    current_age_bucket = dplyr::case_when(
+      .data$current_age < 18 ~ "Under 18",
+      .data$current_age >= 18 & .data$current_age < 25 ~ "18-24",
+      .data$current_age >= 25 & .data$current_age < 35 ~ "25-34",
+      .data$current_age >= 35 & .data$current_age < 45 ~ "35-44",
+      .data$current_age >= 45 & .data$current_age < 55 ~ "45-54",
+      .data$current_age >= 55 & .data$current_age < 65 ~ "55-64",
+      .data$current_age >= 65 ~ "65 and older",
+      TRUE ~ "Unknown"
+    ),
+    # Determine physical custody based on facility, and whehter it's of a
+    # certain type
+    facility_upper = stringr::str_to_upper(.data$facility),
+    physical_custody = .data$facility_upper %in% c(
+      assessment_and_reception,
+      state_institutions,
+      community_centers,
+      halfway_house,
+      private_facilities,
+      interstate
+    ) |
+      stringr::str_detect(.data$facility_upper, "(?i)jail|SHERIFFS OFFICE")
+  ) |>
+  dplyr::select(-c("facility_upper"))
+
+
+# Analysis
+dates <- tibble::tibble(
+  day = seq(ymd("2024-01-01"), ymd("2025-12-31"), by = "1 day")
+) |>
+  dplyr::mutate(day = lubridate::floor_date(day, "hour"))
+
+compute_daily_population <- function(date, intervals) {
+  return(
+    # This is equivalent to sum(date %within% intervals):
+    sum(lubridate::`%within%`(date, intervals), na.rm = TRUE)
+  )
+}
+
+dates |>
+  dplyr::mutate(
+    total_pop = sapply(dates, function(x) {
+      compute_daily_population(x, profile_data$physical_stay)
+    })
+  )
+
+start_date <- ymd("2024-01-01")
+end_date <- ymd("2025-12-31")
+
+daily_population <- profile_data |>
+  transmute(
+    admit_date   = ymd(admit_date),
+    exit_date    = coalesce(ymd(release_date), snapshot_date)
+  ) |>
+  filter(!is.na(admit_date), !is.na(exit_date), admit_date <= exit_date) |>
+    # Construct full name variable
+    dplyr::mutate(
+      # TODO: Reconsider name formatting for consistency with other datasets
+      name = stringr::str_c(
+        dplyr::if_else(!is.na(.data$last_name), stringr::str_c(" ", .data$last_name), ""),
+        dplyr::if_else(!is.na(.data$suffix), stringr::str_c(" ", .data$suffix), ""),
+        ", ",
+        dplyr::if_else(!is.na(.data$first_name), stringr::str_c(" ", .data$first_name), ""),
+        " ",
+        dplyr::if_else(!is.na(.data$middle_name), stringr::str_c(" ", .data$middle_name), ""),
+        "."
+      ),
+      # Construct snapshot_age, current_age, and age_bucket variables based on birth_date
+      snapshot_age = floor(lubridate::time_length(lubridate::interval(.data$birth_date, as.Date(.data$snapshot_date)), "years")),
+      current_age = floor(lubridate::time_length(lubridate::interval(.data$birth_date, Sys.Date()), "years")),
+      current_age_date = today(), # Day the current age was calculated, which can be useful for tracking when age-based analyses were conducted
+      snapshot_age_bucket = dplyr::case_when(
+        .data$snapshot_age < 18 ~ "Under 18",
+        .data$snapshot_age >= 18 & .data$snapshot_age < 25 ~ "18-24",
+        .data$snapshot_age >= 25 & .data$snapshot_age < 35 ~ "25-34",
+        .data$snapshot_age >= 35 & .data$snapshot_age < 45 ~ "35-44",
+        .data$snapshot_age >= 45 & .data$snapshot_age < 55 ~ "45-54",
+        .data$snapshot_age >= 55 & .data$snapshot_age < 65 ~ "55-64",
+        .data$snapshot_age >= 65 ~ "65 and older",
+        TRUE ~ "Unknown"
+      ),
+      current_age_bucket = dplyr::case_when(
+        .data$current_age < 18 ~ "Under 18",
+        .data$current_age >= 18 & .data$current_age < 25 ~ "18-24",
+        .data$current_age >= 25 & .data$current_age < 35 ~ "25-34",
+        .data$current_age >= 35 & .data$current_age < 45 ~ "35-44",
+        .data$current_age >= 45 & .data$current_age < 55 ~ "45-54",
+        .data$current_age >= 55 & .data$current_age < 65 ~ "55-64",
+        .data$current_age >= 65 ~ "65 and older",
+        TRUE ~ "Unknown"
+      ),
+      # Determine physical custody based on facility, and whehter it's of a
+      # certain type
+      facility_upper = stringr::str_to_upper(.data$facility),
+      physical_custody = .data$facility_upper %in% c(
+        assessment_and_reception,
+        state_institutions,
+        community_centers,
+        halfway_house,
+        private_facilities,
+        interstate
+      ) |
+        stringr::str_detect(.data$facility_upper, "(?i)jail|SHERIFFS OFFICE")
+    ) |>
+    dplyr::select(-c("facility_upper"))
+  transmute(
+    start = admit_date,
+    end   = exit_date + days(1)
+  ) |>
+  bind_rows(
+    transmute(., day = start, delta = 1),
+    transmute(., day = end, delta = -1)
+  ) |>
+  summarise(delta = sum(delta), .by = day) |>
+  right_join(tibble(day = seq(start_date, end_date, by = "1 day")), by = "day") |>
+  arrange(day) |>
+  mutate(
+    delta = replace_na(delta, 0L),
+    total_pop = cumsum(delta)
+  ) |>
+  select(day, total_pop)
+
+# NOTE: All of this track Annual + YOY for CY24/25
+# Overall population trends
+population_data <- profile_data |>
+  distinct(doc_num, admit_date, release_date, .keep_all = TRUE) |>
+  benchCalculatePopulation::calculate_population(
+    start = "admit_date",
+    end = "release_date",
+    groups = c("sex", "race")
+  ) |>
+  as_tibble()
+# Population trends, breakdown by gender (male vs. female)
+population_data <- population_data |>
+  mutate(
+    year = lubridate::year(date),
+    month = lubridate::floor_date(date, "month")
+  )
+
+population_data |>
+  summarize(
+    n = sum(n),
+    .by = year
+  )
+# Population trends, breakdown by race/ethnicity
+# Population trends, breakdown by county
+
+# Prison admission - Overall
+profile_data |>
+  filter(n() > 1, .by = c("doc_num", "admit_date"))
+
+profile_data |>
+  distinct(doc_num, admit_date) |>
+  mutate(
+    admit_month = lubridate::floor_date(admit_date, unit = "month"),
+    admit_year = lubridate::year(admit_date),
+  ) |>
+  summarise(
+    n_admissions = n(),
+    .by = admit_year
+  )
+
+# Prison releases - Overall
+profile_data |>
+  mutate(
+    release_month = lubridate::floor_date(release_date, unit = "month"),
+    release_year = lubridate::year(release_date),
+  ) |>
+  filter(release_year < 2024)
+  summarise(
+    n_releases = n(),
+    .by = release_year
+  )
+
+# Prison sentences - Overall
+# Prison sentences - breakdown by county
+
+# Prison recidivism rate - overall
+# Prison recidivism rate by gender
+# Prison recidivism rate by race/ethnicity
+# Prison recidivism rate by county
+
+### Takeaways
+## sentences data
+# INCARCERATED_TERM_IN_YEARS is missing __, __, __. Shows only 999.
+# What is the reason for lack of SENTENCE_START_DATE?
+# Why do some have such high SENTENCE_END_DATE?
+#
